e87fe3fa33ad62c15488419d71fbf97a01248c0e
binbro708 <b86070800@gmail.com>
2022-10-24 22:18:58 +0800

    logo animation done

--- js/all.js
+++ js/all.js
@@ -1,12 +1,18 @@
-const nav=document.querySelector("nav");
+const nav = document.querySelector("nav");
+const logoBig = document.querySelector(".logoBig");
+const slogan=document.querySelector(".slogan")
 function navOn() {
-    if (window.scrollY > 0) {
-      nav.classList.add("navOn");
-    } else {
-      nav.classList.remove("navOn");
-    }
+  if (window.scrollY > 0) {
+    nav.classList.add("navOn");
+    logoBig.classList.add("logoOut");
+    slogan.classList.add("sloganOut")
+  } else {
+    nav.classList.remove("navOn");
+    logoBig.classList.remove("logoOut");
+    slogan.classList.remove("sloganOut")
   }
-  
-  window.addEventListener("scroll", navOn);
-  
+}
+
+window.addEventListener("scroll", navOn);
+
 console.log(nav);
