30e77c6aadd2dd8d376f8ac9dee3c24f1a22c347
binbro708 <b86070800@gmail.com>
2022-10-24 15:55:30 +0800

    school logo animation

--- js/all.js
+++ js/all.js
@@ -1,12 +1,18 @@
-const nav=document.querySelector("nav");
+const nav = document.querySelector("nav");
+const logo = document.querySelector("#logo");
+
 function navOn() {
-    if (window.scrollY > 0) {
-      nav.classList.add("navOn");
-    } else {
-      nav.classList.remove("navOn");
-    }
+  if (window.scrollY > 0) {
+    nav.classList.add("navOn");
+  } else {
+    nav.classList.remove("navOn");
   }
-  
-  window.addEventListener("scroll", navOn);
-  
-console.log(nav);
+}
+
+window.addEventListener("scroll", navOn);
+
+window.onload = () => {
+  logo.classList.add("logoBigOn");
+};
+
+// window.addEventListener("scroll", logoOn);
